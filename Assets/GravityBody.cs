e9583b1fd5a738d5e02546b0b4b901eb6702371c
C.Rondeau <rondeau.clement@gmail.com>
2018-08-22 08:20:53 +0200

    Init project

---
+++ Assets/GravityBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour {
+
+    private GravityAttractor _planet;
+    private Rigidbody _rb;
+    
+    private void Awake()
+    {
+        _planet = GameObject.FindGameObjectWithTag("World").GetComponent<GravityAttractor>();
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+        _rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        _planet.Attract(transform);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody _rb;
+
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+    }
+	
+	void Update () {
+        _rb.velocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            _rb.velocity = transform.forward * 15f;
+        } else if (Input.GetKey(KeyCode.S))
+        {
+            _rb.velocity = transform.forward * -15f;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            _rb.velocity = transform.right * -15f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _rb.velocity = transform.right * 15f;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+}
